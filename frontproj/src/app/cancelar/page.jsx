074ea600a351ac22f7b464fc86dbf16d6d67308f
GabrielRodrS <gsiqueira394@gmail.com>
2025-01-10 16:55:13 -0300

    Implementação CSS de Cancelar reserva

--- frontproj/src/app/cancelar/page.jsx
+++ frontproj/src/app/cancelar/page.jsx
@@ -5,6 +5,7 @@ import Interface from "../../components/Interface";
 import FundoFormulariosInt from "../../components/FundoFormulariosInt";
 import Titulo from "../../components/Titulo";
 import BotaoEnviarInt from "../../components/BotaoEnviarInt";
+import { Check, X } from "lucide-react";
 import InfoReserv from "../../components/InfoReserv";
 
 export default function () {
@@ -13,17 +14,7 @@ export default function () {
   return (
     <Interface>
       <FundoFormulariosInt>
-        <Titulo>Solicitar reserva</Titulo>
-        <label className="flex space-x-2 items-center">
-          <span>Selecionar esporte:</span>
-          <select id="Esporte" className="rounded-sm">
-            <option value="1">Futsal</option>
-            <option value="2">Basquete</option>
-            <option value="3">Vôlei</option>
-            <option value="3">Handebal</option>
-            <option value="3">Outro</option>
-          </select>
-        </label>
+        <Titulo>Solicitar cancelamento</Titulo>
         <h2 className="text-center font-bold mt-5 mb-2">Informações</h2>
         <InfoReserv></InfoReserv>
         <BotaoEnviarInt enviar={enviar}></BotaoEnviarInt>
