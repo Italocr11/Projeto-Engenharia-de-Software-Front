c0dba30583c20838e685716316c95b71518bc549
GabrielRodrS <gsiqueira394@gmail.com>
2025-01-29 13:15:26 -0300

    Implementação de alterarEmail

--- frontproj/src/app/alterarEmail/page.jsx
+++ frontproj/src/app/alterarEmail/page.jsx
@@ -4,12 +4,27 @@ import Interface from "../../components/Interface";
 import Email from "../../components/Email";
 import Titulo from "../../components/Titulo";
 import Senha from "../../components/Senha";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function AlterarEmail() {
   const [senha, setSenha] = useState("");
   const [altEmail, setaltEmail] = useState("");
   const [msg, setMsg] = useState("");
+  const [email, setEmail] = useState(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedEmail = localStorage.getItem("userEmail");
+    if (!storedEmail) {
+      setMsg("Você precisa estar logado!");
+      return;
+    }
+
+    setEmail(storedEmail);
+  }, []);
 
   const confirmar = async (e) => {
     e.preventDefault();
@@ -17,6 +32,24 @@ function AlterarEmail() {
     if (!altEmail || !senha) {
       setMsg("Preencha os campos!");
       return;
+    }
+
+    const dados = { email, novoEmail: altEmail, senha };
+
+    try {
+      const resposta = await axios.patch(
+        "http://localhost:3000/usuarios/altemail",
+        dados
+      );
+
+      const { email: novoEmail } = resposta.data.usuario;
+      localStorage.setItem(
+        "userEmail",
+        resposta.data.usuario?.email || altEmail
+      );
+      router.push("/perfil");
+    } catch (erro) {
+      setMsg(erro.response?.data?.message || "Informações inválidas!.");
     }
   };
   var str = "Novo";
