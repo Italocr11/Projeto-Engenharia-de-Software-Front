a12f5d57f005091f930932a24e48eddb04d6350e
GabrielRodrS <gsiqueira394@gmail.com>
2025-02-22 00:52:36 -0300

    Alterações visuais de design em login (alterações CSS)

--- frontproj/src/components/Titulo.jsx
+++ frontproj/src/components/Titulo.jsx
@@ -1,5 +1,7 @@
 function Titulo({ children }) {
-  return <h1 className="text-center font-bold text-4xl mt-5">{children}</h1>;
+  return (
+    <h1 className="text-center font-bold text-4xl mt-5 mb-5">{children}</h1>
+  );
 }
 
 export default Titulo;
