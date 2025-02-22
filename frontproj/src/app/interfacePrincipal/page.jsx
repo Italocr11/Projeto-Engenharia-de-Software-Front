4b069af99a6b28b77c643d1bd902e46170fa9a9c
GabrielRodrS <gsiqueira394@gmail.com>
2025-02-22 02:06:50 -0300

    Aprimoramento do desgign do header na interface principal

--- frontproj/src/app/interfacePrincipal/page.jsx
+++ frontproj/src/app/interfacePrincipal/page.jsx
@@ -20,9 +20,9 @@ export default function InterfacePrincipal() {
   return (
     <Interface>
       <div className="h-max w-full flex items-center justify-center">
-        <div className="flex flex-col space-y-3 bg-gray-200 w-full pb-6">
+        <div className="flex flex-col space-y-3 bg-stone-200 w-3/6 pb-6">
           <div className="flex items-center justify-center mt-10">
-            <h1 className=" text-3xl mb-5 bold text-yellow-800">
+            <h1 className=" text-3xl mb-5 bold text-amber-800">
               Horários reservados recentemente
             </h1>
           </div>
--- frontproj/src/components/Interface.jsx
+++ frontproj/src/components/Interface.jsx
@@ -17,18 +17,36 @@ export default function Interface({ children }) {
 
   return (
     <div className="bg-white overflow-hidden h-screen" onClick={toggleNotf}>
-      <header className="bg-yellow-400 h-1/6 flex flex-row flex-1 text-black items-center justify-between">
+      <header className="bg-amber-400 h-20 flex flex-row text-blue-950 font-bold text-lg items-center justify-between">
         <button
           onClick={() => {
             router.push("/interfacePrincipal");
           }}
-          className="ml-10 hover:text-gray-700"
+          className="ml-10 hover:text-black"
         >
-          <h1 className="text-lg">Horário para quadras</h1>
+          <h1>Horário para quadras</h1>
         </button>
 
         <button
-          className="hover:text-gray-700"
+          className="flex items-center hover:text-black pt-2"
+          onClick={() => {
+            router.push("./quadro");
+          }}
+        >
+          <Calendar className="mr-2" /> Quadro de horários
+        </button>
+
+        <button
+          className="flex items-center hover:text-black"
+          onClick={() => {
+            router.push("/historico");
+          }}
+        >
+          <Album className="mr-2" /> Histórico de horários
+        </button>
+
+        <button
+          className="hover:text-black"
           onClick={() => {
             setNotf(!notf);
           }}
@@ -40,42 +58,14 @@ export default function Interface({ children }) {
           onClick={() => {
             router.push("/perfil");
           }}
-          className="mr-10 hover:text-gray-700"
+          className="mr-10 hover:text-black"
         >
-          <User className="mb-1 ml-1" />
+          <User className="mb-1 ml-3" />
           <span>Perfil</span>
         </button>
       </header>
-      <div className="flex flex-row flex-1 h-[calc(100vh-6rem)] text-black">
-        <nav className="bg-blue-400 w-1/6 flex flex-col space-y-20 items-center justify-start overflow-y-auto pt-10 ">
-          <button
-            className="flex items-center hover:text-gray-700 pt-2"
-            onClick={() => {
-              router.push("./quadro");
-            }}
-          >
-            <Calendar className="mr-3" /> Quadro de horários
-          </button>
-          <button
-            className="flex items-center hover:text-gray-700"
-            onClick={() => {
-              router.push("/historico");
-            }}
-          >
-            <Album className="mr-3" /> Histórico de horários
-          </button>
-          <button
-            className="flex items-center hover:text-gray-700 bg-blue-500 w-full h-12 justify-center"
-            onClick={() => {
-              router.push("/login");
-            }}
-          >
-            Sair
-          </button>
-        </nav>
-        <main className="bg-gray-100 flex-1 p-10 overflow-y-auto">
-          {children}
-        </main>
+      <div className="flex flex-row flex-1 h-[calc(100vh-5rem)] text-black">
+        <main className=" flex-1 p-10 overflow-y-auto">{children}</main>
       </div>
       {notf && <GuiaNotf></GuiaNotf>}
     </div>
