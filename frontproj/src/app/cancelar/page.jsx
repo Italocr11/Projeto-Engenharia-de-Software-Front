edfd0017fe6d66206b2be5a9ebc4b91b2c439832
GabrielRodrS <gsiqueira394@gmail.com>
2025-02-22 15:23:50 -0300

    Alterações em delete de cancelar reserva

--- frontproj/src/app/cancelar/page.jsx
+++ frontproj/src/app/cancelar/page.jsx
@@ -12,34 +12,31 @@ export default function Cancelar() {
   const router = useRouter();
 
   const [reservaInfo, setReservaInfo] = useState({
-    data: null,
-    horario: null,
-    valor: null,
-    bola: null,
-    rede: null,
-    coletes: null,
+    data: "",
+    horario: "",
+    valor: "",
+    bola: false,
+    rede: false,
+    coletes: false,
   });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const urlParams = new URLSearchParams(window.location.search);
-
-      setReservaInfo({
-        data: urlParams.get("data") || "",
-        horario: urlParams.get("horario") || "",
-        valor: urlParams.get("valor") || "",
-        bola: urlParams.get("bola") === "true",
-        rede: urlParams.get("rede") === "true",
-        coletes: urlParams.get("coletes") === "true",
-      });
-    }
-  }, []);
+    const urlParams = new URLSearchParams(window.location.search);
+    setReservaInfo({
+      data: urlParams.get("data") || "",
+      horario: urlParams.get("horario") || "",
+      valor: urlParams.get("valor") || "",
+      bola: urlParams.get("bola") === "true",
+      rede: urlParams.get("rede") === "true",
+      coletes: urlParams.get("coletes") === "true",
+    });
+  }, [window.location.search]);
 
   async function cancelarRes() {
     try {
+      const queryParams = new URLSearchParams(reservaInfo).toString();
       const resultado = await axios.delete(
-        "http://localhost:3000/usuarios/cancelar",
-        { data: reservaInfo }
+        `http://localhost:3000/usuarios/cancelar?${queryParams}`
       );
 
       if (resultado.status === 200) {
@@ -61,12 +58,14 @@ export default function Cancelar() {
       <FundoFormulariosInt>
         <Titulo>Solicitar cancelamento</Titulo>
         <h2 className="text-center font-bold mt-10 mb-4">Informações:</h2>
-        <InfoReserv {...reservaInfo}></InfoReserv>
+
+        <InfoReserv {...reservaInfo} />
         <button
           type="button"
-          className="py-2 px-4 rounded-md bg-red-400 mt-6"
+          className="py-2 px-4 rounded-md bg-red-400 my-5"
           disabled={!reservaInfo.data}
           onClick={cancelarRes}
+          aria-label="Cancelar reserva"
         >
           Cancelar
         </button>
