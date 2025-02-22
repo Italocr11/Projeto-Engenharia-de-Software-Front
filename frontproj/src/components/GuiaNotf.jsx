5f171ac93b137881f6ed9f68109c438026517c42
GabrielRodrS <gsiqueira394@gmail.com>
2025-02-22 13:52:42 -0300

    Ajustes na aba de notificações

--- frontproj/src/components/GuiaNotf.jsx
+++ frontproj/src/components/GuiaNotf.jsx
@@ -1,9 +1,13 @@
 import Notf from "./Notf";
+import { Check, X } from "lucide-react";
 
 function GuiaNotf() {
   return (
     <div className="h-screen w-screen flex  justify-center">
-      <div className="absolute ml-32 top-20 text-black bg-white p-4 shadow-lg rounded-lg h-1/4 w-2/7">
+      <div className="fixed right-32 top-20 text-black bg-white p-4 shadow-xl rounded-lg space-y-1 w-82 h-3/8">
+        <div className="flex flex-row items-center justify-center bg-black text-white w-full rounded-md p-1 mb-2">
+          <p>------------------ Notificações ------------------</p>
+        </div>
         <Notf></Notf>
       </div>
     </div>
